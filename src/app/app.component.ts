791404a0f60607016832ebed5e2cd8c225bb4ccf
Gabriel Garcia <89267342+gabito1966@users.noreply.github.com>
2022-11-25 11:53:54 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Lista de Empleados';
+
+  empleados: Empleado[] = [
+    new Empleado('Gabriel', 'García', 'Presidente', 800000),
+    new Empleado('Mabel', 'Vizgarra', 'Directora', 600000),
+    new Empleado('Diego', 'Mereles', 'Encargado', 250000),
+    new Empleado('Federico', 'Guiance', 'Vendedor', 300000)
+  ];
 }
